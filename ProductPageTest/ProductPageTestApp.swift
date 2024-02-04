d4d2dca98e4c7ea77bce45d1cc9a8994061e05e3
Aleksey Kuhlenkov <kuhlenkov@gmail.com>
2024-02-04 11:06:33 +0300

    Initial Commit

---
+++ ProductPageTest/ProductPageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductPageTestApp.swift
+//  ProductPageTest
+//
+//  Created by Aleksey Kuhlenkov on 4.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ProductPageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
