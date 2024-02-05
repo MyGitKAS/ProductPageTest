75b2c76cda6737c3ab5111dd22856aae80536676
Aleksey Kuhlenkov <kuhlenkov@gmail.com>
2024-02-05 10:11:57 +0300

    Fixed ProductOriginView

--- ProductPageTest/Components/ProductOriginView.swift
+++ ProductPageTest/Components/ProductOriginView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct ProductOriginView: View {
     var body: some View {
         HStack{
-            Image(systemName: "network")
-                .font(.system(size: 35))
+            Image("flag_bel")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: screen.size.width * 0.1)
             Text("Беларусь, Минск")
             Spacer()
         }
@@ -23,3 +25,4 @@ struct ProductOriginView_Previews: PreviewProvider {
         ProductOriginView()
     }
 }
+
